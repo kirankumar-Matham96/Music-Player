aa8f996f9075142c7a2bf7ecffbe57458d124f33
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-03-25 22:48:48 +0530

    responsive web is in progress

---
+++ musicplayer/script.js
@@ -0,0 +1,59 @@
+// /**
+//  * on clicking the latest release songs, respective song playing ribbon will appear at the bottom of the page.
+//  */
+// const latestSong1 = document
+//   .querySelector("#latest-releases-song-1>img")
+//   .addEventListener("click", () => {
+//     document.querySelector("#music-playing-ribbon-1").style.display = "flex";
+//     document.querySelector("#music-playing-ribbon-2").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-3").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-4").style.display = "none";
+//     console.log("clicked!");
+//   });
+
+// const latestSong2 = document
+//   .querySelector("#latest-releases-song-2>img")
+//   .addEventListener("click", () => {
+//     document.querySelector("#music-playing-ribbon-1").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-2").style.display = "flex";
+//     document.querySelector("#music-playing-ribbon-3").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-4").style.display = "none";
+//     console.log("clicked!");
+//   });
+
+// const latestSong3 = document
+//   .querySelector("#latest-releases-song-3>img")
+//   .addEventListener("click", () => {
+//     document.querySelector("#music-playing-ribbon-1").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-2").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-3").style.display = "flex";
+//     document.querySelector("#music-playing-ribbon-4").style.display = "none";
+//     console.log("clicked!");
+//   });
+
+// const latestSong4 = document
+//   .querySelector("#latest-releases-song-4>img")
+//   .addEventListener("click", () => {
+//     document.querySelector("#music-playing-ribbon-1").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-2").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-3").style.display = "none";
+//     document.querySelector("#music-playing-ribbon-4").style.display = "flex";
+//     console.log("clicked!");
+//   });
+
+// /**
+//  * play or pause the song from the ribbon
+//  */
+// const play1 = document.querySelector(".play");
+// const pause1 = document.querySelector(".pause");
+// play1.style.display = "none";
+
+// pause1.addEventListener("click", () => {
+//   play1.style.display = "block";
+//   pause1.style.display = "none";
+// });
+
+// play1.addEventListener("click", () => {
+//   pause1.style.display = "block";
+//   play1.style.display = "none";
+// });
